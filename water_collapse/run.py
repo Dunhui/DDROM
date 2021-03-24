983badbc56287bd8662210acdddfefee73e00723
Ray <49318840+furui1997@users.noreply.github.com>
2021-03-24 13:55:39 +0000

    Add files via upload

--- water_collapse/run.py
+++ water_collapse/run.py
@@ -1,80 +1,42 @@
-import os
 import sys
-sys.path.append("/home/ray/.virtualenvs/venv_p3/lib/python3.6/site-packages")
-import vtktools
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
-import math
-import keras.backend as K
-import process_data as Pcd
-import mkdirs
-from keras.layers import Input, Dense, LSTM, Dropout
-from keras import regularizers
-from keras.models import Model, Sequential, load_model
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error,mean_absolute_error
-from keras.callbacks import LearningRateScheduler
+sys.path.append("..")
+from Models.AE_model import *
+# from Models.Load_Data import *
+# from Models.Model_Processing import *
 
-def variable_value():
-	path =  "/home/ray/Downloads/fluidity-master/examples/water_collapse"
-	vertify_rate = 0.4
-	sequence_length = 50
-	originalFile =  "/home/ray/Downloads/fluidity-master/examples/water_collapse_vertify" # original file
-	destinationFile = "/home/ray/Downloads/fluidity-master/examples/water_collapse_predict" 
+if __name__ == '__main__':
+	# data
+	path = '../../datasets/data_water_collapse/'	
+	ori_path = path + 'origin_data'
+	fileName = '/water_collapse_'
+	field_name = 'Water::MaterialVolumeFraction'
+	di = 1
+	data_file_name = '../MaterialVol%.npy'
 
-	return path, vertify_rate, sequence_length, originalFile, destinationFile
+	# AE
+	ae_encoding_dim = 8
+	ae_epochs = 1000
+	ae_batch_size = 64
+	modelsFolder = './saved_models'
+	encoderFileName = 'AE_encoder_8.h5'
+	decoderFileName = 'AE_decoder_8.h5'
+	AEFileName = 'AE_8.h5'
+	destinationFolder = path +'ROM_outputs'
+	newFieldName = 'MaterialVol_dim_8'
+	Trans_code_name = 'Transformer_code_8.npy'
 
-def predict_sequences_multiple(model, data, sequence_length, predict_num):
+	# AE training
+	AE(path, ori_path, fileName, field_name, di, data_file_name, ae_encoding_dim, ae_epochs, ae_batch_size, modelsFolder,encoderFileName, decoderFileName, AEFileName, destinationFolder, newFieldName, Trans_code_name)
+
+	seq_len = 128
+	d_k = 256
+	d_v = 256
+	n_heads = 12
+	ff_dim = 256
+	trans_batch_size = 256
+	trans_epochs=300
+	transformer_model = 'Trans_model.h5'
+	transformer_outputs = 'output_transformer.npy'
+
 	
-	data_origin = data[data.shape[0] - sequence_length + 1:,:]
-	data = data_origin.reshape(1,data_origin.shape[0],data_origin.shape[1]) 
 
-	print('[Model] Predicting Sequences Multiple...')
-	for i in range(predict_num):
-		list_p = data[:,:,:] if i == 0 else data[:,i:,:]
-		code = model.predict(list_p)
-		code = code.reshape(1,1,code.shape[1])
-		data = np.concatenate((data,code), axis = 1) 
-	data = data.reshape(data.shape[1],data.shape[2])
-	return data[sequence_length-1:,:]
-
-def predict_vol(dataset, predict_num, sequence_length):
-
-	print(np.max(dataset),np.min(dataset), np.mean(dataset), np.median(dataset))
-
-	encoder = load_model('vol_encoder.h5', compile = False) # encoder test data
-	code = encoder.predict(dataset)
-	print(np.max(code),np.min(code),np.mean(code), np.median(code), code.shape)
-
-	LSTM = load_model('vol_lstm.h5', compile=False)
-	outputs = predict_sequences_multiple(LSTM, code, sequence_length, predict_num)
-	print(np.max(outputs),np.min(outputs),np.mean(outputs), np.median(outputs), outputs.shape)
-
-	decoder = load_model('vol_decoder.h5', compile = False) # decoder predicted data
-	predicted_vol = decoder.predict(outputs)
-	print(np.max(predicted_vol),np.min(predicted_vol),np.mean(predicted_vol), np.median(predicted_vol))
-
-	return predicted_vol
-
-if __name__=="__main__":  
-
-	path, vertify_rate, sequence_length, originalFile, destinationFile = variable_value()
-
-	print("Data Preprocessing...")  
-	vol = Pcd.get_data(path)
-	dataset, vertify = Pcd.train_and_vertify(vol,vertify_rate) 
-	scaler_data = MinMaxScaler()
-	scaler_vol = scaler_data.fit_transform(dataset)
-
-	print("Data Predicting...")  
-	predicted_vol = predict_vol(scaler_vol,vertify.shape[0],sequence_length)
-	scaler_outpus = scaler_data.inverse_transform(predicted_vol)
-
-	value = mean_squared_error(vertify, scaler_outpus)
-	print(value)
-
-	mkdirs.mkdir(destinationFile)
-	mkdirs.copyFiles(originalFile,destinationFile)
-	vtu_num = Pcd.get_vtu_num(originalFile)
-	mkdirs.transform(scaler_outpus, vtu_num, destinationFile)
